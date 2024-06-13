c7932bc9dd89d9bd239553770f29d9993212f1c1
José de Jesús Hernández Vázquez <johernandezvaz@gmail.com>
2024-06-13 16:20:18 -0600

    Cambio de puerto

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", async function() {
-  const response = await fetch("http://localhost:4242/config");
+  const response = await fetch("http://localhost:8080/config");
   const config = await response.json();
-
+  const port = config.port;
   const stripe = Stripe(config.publicKey);
   const items = [{ id: "Boleto" }];
   let elements;
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", async function() {
 
   async function initialize() {
     try {
-      const response = await fetch("http://localhost:4242/create-payment-intent", {
+      const response = await fetch(`http://localhost:${port}/create-payment-intent`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ items }),
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", async function() {
       const { error, paymentIntent } = await stripe.confirmPayment({
         elements,
         confirmParams: {
-          return_url: "http://localhost:4242/checkout.html",
+          return_url: "http://localhost:8080/checkout.html",
         },
         redirect: "if_required"
       });
