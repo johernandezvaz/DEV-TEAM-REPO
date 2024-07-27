1901b33085880a98281ad6f02ec6fbddf22f187e
Guille Zacarias <guillezacariaso@gmail.com>
2024-07-27 17:57:37 -0300

    algunos cambios

--- scripts/boletos.js
+++ scripts/boletos.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const ticketOptions = document.querySelectorAll('.ticket-option');
+document.addEventListener("DOMContentLoaded", function () {
+  const ticketOptions = document.querySelectorAll(".ticket-option");
 
-  ticketOptions.forEach(option => {
-      option.addEventListener('click', function() {
-          // Remove 'selected' class from all options
-          ticketOptions.forEach(opt => opt.classList.remove('selected'));
+  ticketOptions.forEach((option) => {
+    option.addEventListener("click", function () {
+      // Remove 'selected' class from all options
+      ticketOptions.forEach((opt) => opt.classList.remove("selected"));
 
-          // Add 'selected' class to the clicked option
-          this.classList.add('selected');
+      // Add 'selected' class to the clicked option
+      this.classList.add("selected");
 
-          // Update the radio button state
-          const input = this.querySelector('input[type="radio"]');
-          if (input) {
-              input.checked = true;
-          }
-      });
+      // Update the radio button state
+      const input = this.querySelector('input[type="radio"]');
+      if (input) {
+        input.checked = true;
+      }
+    });
   });
 });
