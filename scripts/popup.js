5cb4947d74bceb1dff8a9f56a6f69be5f8970ba1
johernandezvaz <johernandezvaz@gmail.com>
2024-08-06 10:46:07 -0600

    Popup fix

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,20 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const openPopup = document.getElementById('openPopup');
-    const popup = document.getElementById('popup');
-    const closePopup = document.getElementById('closePopup');
+    const openPopup = document.getElementById('openPopop');
+    const popup = document.getElementById('popop');
+    const closePopup = document.getElementById('closePopop');
+    const comprarBoleto= document.getElementById('comprarEntrada');
+    const body = document.body;
 
-    openPopup.addEventListener('click', function(e) {
-        e.preventDefault();
-        popup.style.display = 'flex';
-    });
+    if (openPopup && popup && closePopup && comprarBoleto) {
+        openPopup.addEventListener('click', function(e) {
+            e.preventDefault();
+            popup.style.display = 'flex';
+            body.classList.add('no-scroll');
+        });
 
-    closePopup.addEventListener('click', function() {
-        popup.style.display = 'none';
-    });
-
-    window.addEventListener('click', function(e) {
-        if (e.target == popup) {
+        closePopup.addEventListener('click', function() {
             popup.style.display = 'none';
-        }
-    });
+            body.classList.remove('no-scroll');
+        });
+
+        comprarBoleto.addEventListener('click', function() {
+            popup.style.display = 'none';
+            body.classList.remove('no-scroll');
+        });
+
+        window.addEventListener('click', function(e) {
+            if (e.target == popup) {
+                popup.style.display = 'none';
+                body.classList.remove('no-scroll');
+            }
+        });
+    } else {
+        console.error('Elementos no encontrados.');
+    }
 });
