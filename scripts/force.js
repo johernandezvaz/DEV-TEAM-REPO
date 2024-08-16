61da2c810cb22c6defcbb4c3a50ab48fc02ed05d
johernandezvaz <johernandezvaz@gmail.com>
2024-08-15 18:31:07 -0600

    Force HTTPS

---
+++ scripts/force.js
@@ -0,0 +1,3 @@
+if (window.location.protocol !== "https:" || window.location.hostname !== "www.codecuu.com") {
+    window.location.href = "https://www.codecuu.com" + window.location.pathname + window.location.search;
+}
