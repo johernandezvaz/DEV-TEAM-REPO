11f6441fc7c87c1b2a326d09edfaf58f1a2e45a8
johernandezvaz <johernandezvaz@gmail.com>
2024-09-09 19:42:12 -0600

    Pixel add

--- scripts/server.js
+++ scripts/server.js
@@ -2,6 +2,39 @@ const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
 
+'use strict';
+const bizSdk = require('facebook-nodejs-business-sdk');
+const ServerEvent = bizSdk.ServerEvent;
+const EventRequest = bizSdk.EventRequest;
+const UserData = bizSdk.UserData;
+const CustomData = bizSdk.CustomData;
+const Content = bizSdk.Content;
+
+const access_token =  process.env.ACCESS_TOKEN;
+const pixel_id = process.env.PIXEL_ID;
+const api = bizSdk.FacebookAdsApi.init(access_token);
+
+let current_timestamp = Math.floor(new Date() / 1000);
+
+const userData_0 = (new UserData())
+    .setEmails(["7b17fb0bd173f625b58636fb796407c22b3d16fc78302d79f0fd30c2fc2fc068"])
+    .setPhones([]);
+const customData_0 = (new CustomData())
+    .setValue(2000)
+    .setCurrency("MXN");
+const serverEvent_0 = (new ServerEvent())
+    .setEventName("Purchase")
+    .setEventTime(1725931888)
+    .setUserData(userData_0)
+    .setCustomData(customData_0)
+    .setActionSource("website");
+
+const eventsData = [serverEvent_0];
+const eventRequest = (new EventRequest(access_token, pixel_id))
+    .setEvents(eventsData);
+eventRequest.execute();
+
+
 const app = express();
 const port = process.env.PORT || 8080;
 const stripe = require("stripe")(process.env.SECRET_KEY);
